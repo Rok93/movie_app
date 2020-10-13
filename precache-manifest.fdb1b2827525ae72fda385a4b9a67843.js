77b5982c63f2ba55303e96ec48fe3e333850aece
김경록 <goodboy3421@gmail.com>
2020-10-13 20:04:43 +0900

    Updates

---
+++ precache-manifest.fdb1b2827525ae72fda385a4b9a67843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7afde88599fa0edc596d255e2b51fba",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "deac8e1b48737ce896c5",
+    "url": "/movie_app/static/css/main.14953f4d.chunk.css"
+  },
+  {
+    "revision": "f5e3ed7c75c4a25217cd",
+    "url": "/movie_app/static/js/2.0b8b08e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.0b8b08e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deac8e1b48737ce896c5",
+    "url": "/movie_app/static/js/main.545efa55.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
